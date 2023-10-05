038e2572d77075321ff607041df5d19027a5dfa4
blordeus <imaginedbybryan@gmail.com>
2023-10-05 14:30:01 -0400

    Complete Project

---
+++ .history/todo-app/components/StatusBar_20231005141753.jsx
@@ -0,0 +1,67 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react"
+
+export const StatusBar = ({colorMode, itemLeft, handleClearAllClick, handleAllClick, handleActiveClick, handleCompletedClick}) => {
+    const [isMobileView, setIsMobileView] = useState(false);
+
+    useEffect(() => {
+        function handleResize() {
+            if (window.innerWidth < 700) setIsMobileView(true);
+            else setIsMobileView(false);
+        }
+
+        window.addEventListener("resize", handleResize);
+        handleResize();
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        }
+    }, [])
+  return (
+    <Box fontWeight={"700"} color={"grey"}>
+        {isMobileView ? (<Flex direction={"column"}>
+            <Flex h={"3em"} justifyContent={"space-between"} alignItems={"center"} p={"1.2em"} backgroundColor={colorMode === "light" ? "white" : "#1a202c"} borderBottomRadius={"10px"}>
+            <Text>
+                {itemLeft} {itemLeft > 1 ? "items" : "item"} left 
+                </Text>
+                <Text onClick={handleClearAllClick} cursor={"pointer"} _hover={{color: colorMode === "light" ? "black" : "white"}}>
+                    Clear Completed
+                </Text>
+            </Flex>
+            <Box mt="3em" h="3em" minW={"100%"} backgroundColor={"#2424"}>
+                <Flex gap={"1em"} h={"3em"} justifyContent={"center"} alignItems={"center"} borderRadius={"10px"} backgroundColor={colorMode === "light" ? "white" : "#1a202c"}>
+                <Text onClick={handleAllClick} cursor={"pointer"} _hover={{color: colorMode === "light" ? "black" : "white"}}>
+                    All
+                </Text>
+                <Text onClick={handleActiveClick} cursor={"pointer"} _hover={{color: colorMode === "light" ? "black" : "white"}}>
+                    Active
+                </Text>
+                <Text onClick={handleCompletedClick} cursor={"pointer"} _hover={{color: colorMode === "light" ? "black" : "white"}}>
+                    Completed
+                </Text>
+                </Flex>
+            </Box>
+            </Flex>): (
+                <Flex p={"1.2em"} w={"100%"} justifyContent={"space-between"} backgroundColor={colorMode === "light" ? "white" : "#1a202c"} borderBottomRadius={"10px"}>
+                <Text>
+                    {itemLeft} {itemLeft > 1 ? "items" : "item"} left
+                </Text>
+                <Text onClick={handleClearAllClick} cursor={"pointer"} _hover={{color: colorMode === "light" ? "black" : "white"}}>
+                    Clear Completed
+                </Text>
+                <Flex gap={"1em"} justifyContent={"center"} alignItems={"center"} borderRadius={"10px"} backgroundColor={colorMode === "light" ? "white" : "#1a202c"}>
+                <Text onClick={handleAllClick} cursor={"pointer"} _hover={{color: colorMode === "light" ? "black" : "white"}}>
+                    All
+                </Text>
+                <Text onClick={handleActiveClick} cursor={"pointer"} _hover={{color: colorMode === "light" ? "black" : "white"}}>
+                    Active
+                </Text>
+                <Text onClick={handleCompletedClick} cursor={"pointer"} _hover={{color: colorMode === "light" ? "black" : "white"}}>
+                    Completed
+                </Text>
+                </Flex>
+            </Flex>
+            )}
+    </Box>
+  )
+}
---
+++ .history/todo-app/src/App_20231005135319.jsx
@@ -0,0 +1,149 @@
+import { useEffect, useState } from "react";
+import { Box, useColorMode } from "@chakra-ui/react";
+import { v4 } from "uuid";
+import lightBackgroundImage from "./images/bg-desktop-light.jpg";
+import darkBackgroundImage from "./images/bg-desktop-dark.jpg";
+import mobileLightImage from "./images/bg-mobile-light.jpg"
+import mobileDarkImage from "./images/bg-mobile-dark.jpg"
+import Header from "../components/Header";
+import { InputButton } from "../components/InputButton";
+import { addTodo, fetchTodos, markToDoCompleted, deleteTodo, countUncompletedTodo, clearAllCompletedTodos, getActiveTodos, getCompletedTodos } from "./actions";
+import { TodoList } from "../components/TodoList";
+import { StatusBar } from "../components/StatusBar";
+
+function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [itemLeft, setItemLeft] = useState(0);
+
+const lightBG = () => {
+  //TODO: Toggle between desktop image and mobile image based on desktop size
+  const desktopImage = lightBackgroundImage;
+  const mobileImage = mobileLightImage;
+  const screenSize = window.screen.width >= 900px;
+
+  const backgroundImage = screenSize ? desktopImage : mobileImage;
+  
+  const bodyStyle = document.body.style;
+  bodyStyle.backgroundImage = `url(${backgroundImage})`;
+  bodyStyle.backgroundRepeat = 'no-repeat';
+  bodyStyle.backgroundSize = 'contain';
+}
+
+const darkBG = () => {
+  const desktopImage = darkBackgroundImage;
+  const mobileImage = mobileDarkImage;
+  const screenSize = window.innerWidth;
+
+  const backgroundImage = screenSize > 575 ? desktopImage : mobileImage;
+
+  const bodyStyle = document.body.style;
+
+  bodyStyle.backgroundImage = `url(${backgroundImage})`;
+  bodyStyle.backgroundRepeat = 'no-repeat';
+  bodyStyle.backgroundSize = 'contain';
+
+}
+
+  const handleAddTodo = async (e) => {
+    if (todo && e.keyCode == 13) {
+      const newTodo = {
+        id: v4(),
+        title: todo,
+        isCompleted: false,
+      };
+      await addTodo(newTodo);
+      setTodos(await fetchTodos());
+      setTodo("");
+    }
+  };
+
+  const handleCompletedTodo = async (id) => {
+    await markToDoCompleted(id);
+    countUncompletedTodo().then((count) => setItemLeft(count));
+  };
+
+  const handleDeleteTodo = async (id) => {
+    await deleteTodo(id);
+    setTodos(await fetchTodos());
+  };
+
+  const handleClearAllClick = async () => {
+    await clearAllCompletedTodos();
+    fetchTodos().then((data) => setTodos(data));
+  }
+
+  const handleAllClick = async () => {
+    fetchTodos().then((data) => setTodos(data));
+  }
+
+  const handleActiveClick = async () => {
+    getActiveTodos().then(todos => setTodos(todos));
+  }
+
+  const handleCompletedClick = async () => {
+    getCompletedTodos().then(todos => setTodos(todos));
+  }
+
+  useEffect(() => {
+    fetchTodos().then((data) => setTodos(data));
+  }, []);
+  
+  useEffect(() => {
+    countUncompletedTodo().then((count) => setItemLeft(count));
+  }, [todos]);
+
+  return (
+    <>
+      <Box
+        backgroundImage={
+          colorMode === "light" ? lightBG : darkBG
+        }
+        backgroundSize="contain"
+        backgroundRepeat="no-repeat"
+        h={"40vh"}
+      >
+        <Box w={{base: "80%", md: "60%", lg: "40%"}} m={"auto"} p="4em 0" >
+          <Header colorMode={colorMode} toggleColorMode={toggleColorMode} />
+          <InputButton
+            colorMode={colorMode}
+            todo={todo}
+            setTodo={setTodo}
+            addTodo={handleAddTodo}
+          />
+        </Box>
+      </Box>
+      <Box
+        h={"60vh"}
+        backgroundSize={"cover"}
+        background={"#242424"}
+        position={"relative"}
+      >
+        <Box
+          minW={"100%"}
+          m={"auto"}
+          position={"absolute"}
+          top={"-10"}
+        >
+          <Box w={{base: "80%", md: "60%", lg: "40%"}} m={"auto"}>
+          <Box maxH={"50vh"} overflowY={"auto"} borderTopRadius={"10px"} backgroundColor={colorMode === "light" ? "white" : "#1a202c"}>
+            <TodoList
+              todos={todos}
+              colorMode={colorMode}
+              handleCompletedTodo={handleCompletedTodo}
+              handleDeleteTodo={handleDeleteTodo}
+            />
+          </Box>
+          <StatusBar colorMode={colorMode} itemLeft={itemLeft} handleClearAllClick={handleClearAllClick}  handleAllClick={handleAllClick}
+              handleActiveClick={handleActiveClick}
+              handleCompletedClick={handleCompletedClick}/>
+          </Box>
+     
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default App;
